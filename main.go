dae4573931c13e2efac9664b83e64cd6594ef0b1
Rabi Gautam <rabi@sharelookapp.com>
2022-04-30 17:37:02 +0545

    impl:handle function with aws lamda

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 import(
-	
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
 )
-func main(){
-	
+type Event struct{
+Name string `json:"what is your name?"`
+Age int `json:"How old are you?"`
 }
+type Response struct{
+	Message string `json:"Answer"`
+}
+
+func HandlerLambdaEvent(event Event)(Response, error){
+
+	return Response{Message: fmt.Sprintf("%s is %d years old",event.Name,event.Age)},nil
+}
+func main(){
+	fmt.Println("here code goes")
+
+	lambda.Start(HandlerLambdaEvent)
+}
+
